1f436e7b68416138b8534f822561f19cf4f8a168
Daniel Ugbomah <danielugbomaht@gmail.com>
2025-04-07 08:30:12 +0100

    Connected a dotenv file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+load_dotenv()
 
 @app.route('/')
 def hello():
-  return "Welcome to the 30 days challenge"
+  return "Welcome to my 30 days challenge"
 
 if __name__ == '__main__':
   app.run(debug=True)
