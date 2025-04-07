27da1f813d2b3a0e07817f101616993ec4ca3bdf
Daniel Ugbomah <danielugbomaht@gmail.com>
2025-04-07 08:15:01 +0100

    Changed return message

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def hello():
-  return "Hello World!!!"
+  return "Welcome to the 30 days challenge"
 
 if __name__ == '__main__':
   app.run(debug=True)
