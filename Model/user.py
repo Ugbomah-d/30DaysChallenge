bd78b0f6e258a786577b50be4ce081c86e45e1db
Daniel Ugbomah <danielugbomaht@gmail.com>
2025-05-05 11:55:55 +0100

    Restarted

---
+++ Model/user.py
@@ -0,0 +1,74 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+  id = db.Column(db.Integer, primary_key= True)
+  username = db.Column(db.String(80), unique=True, nullable = False)
+  email = db.Column(db.String(80), unique=True, nullable = False)
+  firstname =db.Column(db.String(80))
+  lastname =db.Column(db.String(80))
+  password =db.Column(db.String(80))
+  course =db.Column(db.String(80))
+
+def __repr__(self):
+  return f'<User {self.username}>'
+
+
+# CREATE TABLE IF NOT EXISTS users (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   username VARCHAR,
+#   firstname VARCHAR,
+#   lastname VARCHAR,
+#   passwords VARCHAR,
+#   email VARCHAR,
+#   isVerified BOOLEAN,
+#   course VARCHAR,
+#   created_at TIMESTAMP,
+#   updated_at TIMESTAMP
+# );
+
+# CREATE TABLE IF NOT EXISTS posts (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   title VARCHAR,
+#   body TEXT,
+#   media_link VARCHAR,
+#   created_at TIMESTAMP,
+#   updated_at TIMESTAMP,
+#   user_id INTEGER,
+#   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+# );
+
+# CREATE TABLE IF NOT EXISTS likes (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   user_id INTEGER,
+#   post_id INTEGER,
+#   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+#   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+# );
+
+# CREATE TABLE IF NOT EXISTS dislikes (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   user_id INTEGER,
+#   post_id INTEGER,
+#   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+#   FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE
+# );
+
+# CREATE TABLE IF NOT EXISTS otp (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   otp VARCHAR,
+#   expiry_time TIMESTAMP,
+#   created_at TIMESTAMP,
+#   user_id INTEGER,
+#   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+# );
+
+# CREATE TABLE IF NOT EXISTS followers (
+#   id INTEGER PRIMARY KEY AUTOINCREMENT,
+#   follower_id INTEGER,
+#   following_id INTEGER,
+#   created_at TIMESTAMP,
+#   FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+#   FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE
+# );
--- app.py
+++ app.py
@@ -1,57 +1,24 @@
 from flask import Flask, g, request, jsonify
 from dotenv import load_dotenv
 from werkzeug.security import generate_password_hash, check_password_hash
-import os
-import sqlite3
+from Model.user import db, User
 
 app = Flask(__name__)
 load_dotenv()
-DATABASE = 'database.db'
+# DATABASE = 'database.db'
 
-#Connection to database
-def get_db():
-  if 'db' not in g:
-    g.db = sqlite3.connect(DATABASE)
-    g.db.row_factory = sqlite3.Row
-  return g.db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# db = get_db()
+db.init_app(app)
+
+with app.app_context():
+  db.create_all()
 
 @app.route('/')
 def hello():
-  return "Welcome to my 30 days challenges"
-
-# @app.route('/users')
-# def get_users():
-#   db = get_db()
-#   user = db.execute("SELECT * FROM users").fetchall()
-#   user_list = [dict(row) for row in user]  # convert rows to dictionaries
-#   return jsonify(user_list)
-
-#Registration route
-@app.route('/register', methods=['POST'])
-def register():
-  if request.method == 'POST':
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    db = get_db()
-    
-    db.execute("Insert into users(username, passwords) Values (?,?)",(username, generate_password_hash(password) ))
-    db.commit()
-  return "Registered"
-
-#Login route
-@app.route('/login', methods=['POST'])
-def login():
-  if request.method == 'POST':
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-
-    db = get_db()
-      
+  return 'Welcome'
 
 if __name__ == '__main__':
+  
   app.run(debug=True)
